737d9311bbc92855d7a3212d44fd62d4a643d1db
Ashwanth <ashwanthbalakrishnan5@gmail.com>
2023-05-14 21:41:53 +0530

    update requriements.txt

--- web-app/app.js
+++ web-app/app.js
@@ -23,12 +23,12 @@ app.listen(3000, ()=>{
     console.log("listening to port : 3000")
 })
 
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-       console.log(xhttp.responseText)
-    }
-};
-xhttp.open("GET", "filename", true);
-xhttp.send();
+// var xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//        // Typical action to be performed when the document is ready:
+//        console.log(xhttp.responseText)
+//     }
+// };
+// xhttp.open("GET", "filename", true);
+// xhttp.send();
