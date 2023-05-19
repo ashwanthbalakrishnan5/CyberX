70c74c92f02d5531658ef0b80f75de810b314ec6
Ashwanth Balakrishnan <ashwanthbalakrishnan5@gmail.com>
2023-05-20 03:18:26 +0530

    updated face reg

--- ArgusAPI/API/predict_face.py
+++ ArgusAPI/API/predict_face.py
@@ -3,13 +3,23 @@ import face_recognition
 import os
 import pickle
 import json
+import django
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ArgusAPI.settings')
+django.setup()
+
+from ArgusAPI.settings import STORAGE_ROOT
 
 def predict(input_img):
     input_face_locations = face_recognition.face_locations(input_img)
     input_face_encodings = face_recognition.face_encodings(input_img, input_face_locations)
         
-    with open('media/facedata/set_of_encodings.pkl', 'rb') as f:
+    with open(STORAGE_ROOT+'/set_of_encodings.pkl', 'rb') as f:
         set_of_encodings = pickle.load(f)
+    with open(STORAGE_ROOT+'/set_of_images.pkl', 'rb') as f:
+        set_of_images = pickle.load(f)
         
     results = []
     for encodings in set_of_encodings:
